63767dc6cb95a25748f8fc82390ca89f7ad290af
Serhii Yakubovych <yasyncsm@gmail.com>
2017-02-18 15:23:09 +0200

    Google map and mobile version are ready.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
+;(function() {
+    function initPage() {
+        document.addEventListener('click', hideMessageForm);
+    }
+    function initMap() {
+        var uluru = {lat: 50.446159, lng: 30.515426};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+    function hideMessageForm(event) {
+        /**
+         * 'event.target' have to be a hide form button
+         * then 'event.target.parentNode' is a box containing a message form
+         */
+        if ( !event.target.classList.contains("hide-form-button") ||
+             !event.target.parentNode.classList.contains("message-box") ) {
+            return;
+        }
+        event.target.parentNode.classList.toggle("hide-form");
+    }
+
+    window.addEventListener("DOMContentLoaded", initPage);
+    window.RapidSinglePage = {
+        initMap: initMap
+    };
+}());
